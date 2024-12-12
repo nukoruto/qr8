f17ce099557172af76445914b1be810f78a4fc65
孔明 小形 <df360a@gmail.com>
2024-12-12 21:52:54 +0900

    qr_code_scannerからmobile_scannerに変更し、実機デバッグを可能にしました

--- lib/screens/qr_scanner.dart
+++ lib/screens/qr_scanner.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 import 'file_viewer.dart';
 
 class QRScannerScreen extends StatefulWidget {
@@ -8,24 +8,35 @@ class QRScannerScreen extends StatefulWidget {
 }
 
 class _QRScannerScreenState extends State<QRScannerScreen> {
-  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  final MobileScannerController scannerController = MobileScannerController();
+
+  @override
+  void dispose() {
+    scannerController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('QRコードスキャナー')),
-      body: QRView(
-        key: qrKey,
-        onQRViewCreated: (QRViewController controller) {
-          controller.scannedDataStream.listen((scanData) {
-            controller.pauseCamera();
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(
-                builder: (context) => FileViewerScreen(filePath: scanData.code!),
-              ),
-            );
-          });
+      body: MobileScanner(
+        controller: scannerController,
+        onDetect: (capture) {
+          final List<Barcode> barcodes = capture.barcodes;
+          for (final barcode in barcodes) {
+            if (barcode.rawValue != null) {
+              scannerController.stop();
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      FileViewerScreen(filePath: barcode.rawValue!),
+                ),
+              );
+              break;
+            }
+          }
         },
       ),
     );
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,12 @@
 import FlutterMacOS
 import Foundation
 
-import open_file_mac
+import mobile_scanner
 import path_provider_foundation
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
